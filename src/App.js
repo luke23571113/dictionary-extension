d09f2b23fde10333cea03cd21eb9575d881efaa9
Cerberus100 <luke23571113@gmail.com>
2022-01-28 13:40:23 -0800

    need to build an express server ...

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  //api variables:
+  //TODO: can't make api requests from client side, pass the info onto personal sever(python?) 
+  //then call it from there
+  //create a proxy server to call the api
+
   const app_id = "09f998f8"
   const app_key = "cf6f5b4fc1a75f2126b3c1ffc2e402d2"
   const language = "en-gb"
@@ -13,27 +16,6 @@ const App = () => {
   const headers_list = { "app_id" : app_id, "app_key" : app_key };
   const url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.toLowerCase();
 
-  useEffect ( () => {
-    fetch(url, { crossDomain : true, method: "POST", mode : "cors", headers : headers_list} )
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(error => console.log("Error: " + error.message));
-
-    // .then(res => res.json())
-    // .then(
-    //   (result) => {
-    //     setIsLoaded(true);
-    //     setResult(result);
-    //     setIsError(false);
-    //   },
-    //   ( error ) => {
-    //     setIsLoaded(true);
-    //     setIsError(true);
-    //     console.log(error.message);
-    //   }
-    // )
-  }, []);
-  
   return (
     <h1>hello</h1>
   );
